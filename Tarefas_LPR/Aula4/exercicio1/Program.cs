f4d0e9f44b9d389785cd7f632fe95cf8c0613484
Danielbalbino01 <xdaniel900x@outlook.com>
2025-03-21 09:49:39 -0300

    tarefa

---
+++ Tarefas_LPR/Aula4/exercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+                Console.Write("Digite o número:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if (n % 2 == 0)
+                {
+                    Console.WriteLine($"{n} é um número par.");
+                }
+                else
+                {
+
+                    Console.WriteLine($"{n} não é um número par.");
+                }
+            
